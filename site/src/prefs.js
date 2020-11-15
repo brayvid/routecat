4eb6aa60d5d59f5b80ec11ae0b923158e385d42d
Blake Rayvid <brayvid@users.noreply.github.com>
2020-11-15 13:08:51 -0500

    update

--- site/src/prefs.js
+++ site/src/prefs.js
@@ -1,13 +1,13 @@
-    initialInputRows = 6,
+initialInputRows = 6,
 
-        transportation = 'BICYCLING', // or 'DRIVING' or 'WALKING'
+transportation = 'BICYCLING', // or 'DRIVING' or 'WALKING'
 
-        dropoffSeconds = 30, // rough time stopped at a waypoint
+dropoffSeconds = 30, // rough time stopped at a waypoint
 
-        defaultColors = [
-            "#e6194B",
-            "#3cb44b",
-            "#4363d8",
-            "#9A6324",
-            "#f032e6"
-        ];
+defaultColors = [
+    "#e6194B",
+    "#3cb44b",
+    "#4363d8",
+    "#9A6324",
+    "#f032e6"
+];
