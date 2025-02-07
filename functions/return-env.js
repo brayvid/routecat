d73e708b5adf2c05903c0eddf00ab2c92269dd27
Blake Rayvid <brayvid@users.noreply.github.com>
2025-02-07 16:22:59 -0500

    remove print statement

--- functions/return-env.js
+++ functions/return-env.js
@@ -1,6 +1,5 @@
 exports.handler = function(event, context, callback) {
   const { API_KEY } = process.env;
-  console.log(API_KEY);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ token: API_KEY }),
